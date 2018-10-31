05c391703e3c91b638e3090523a881b93107769d
Max Harper <maxharp3r@gmail.com>
2018-10-31 10:30:29 -0500

    dynamic nav

--- src/views/ThirdPage.vue
+++ src/views/ThirdPage.vue
@@ -3,5 +3,9 @@
     <h1>this is a third page</h1>
     <p>URL path parameter: {{ $route.params.name }}</p>
     <p>URL query parameter: {{ $route.query.a }}</p>
+
+    <button @click="$router.push({name: 'home'})">navigate to home</button>
+    <br>
+    <button @click="$router.push({name: 'third', params: {name: 'qwer1234'}, query: {a: 22}})">navigate fancy</button>
   </div>
 </template>
